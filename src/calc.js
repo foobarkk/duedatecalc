475576c06c0ce4bb276ba032bc3f4cdea761eeca
Katalin Kiss <katalin@ghostmonitor.com>
2018-08-22 14:49:02 +0200

    completed parameter validation

--- src/calc.js
+++ src/calc.js
@@ -1,6 +1,6 @@
 module.exports = {
   calc: (ticketCreatedAt, turnaroundHours) => {
-    if (!turnaroundHours) throw new Error('One parameter is missing!')
+    if (isNaN(turnaroundHours) || !turnaroundHours > 0) throw new Error('Turnaround is not a positive number')
 
     const startTime = new Date(ticketCreatedAt)
     if (!(startTime instanceof Date) || isNaN(startTime.valueOf())) throw new Error('First parameter must be a date')
--- test/calc.test.js
+++ test/calc.test.js
@@ -3,14 +3,18 @@
 
 const calculator = require('../src/calc')
 
-test.skip('should not throw error when called woth two parameters', () => {
-  expect(calculator.calc('alma', 'korte')).toBeTruthy()
-})
-
 test('should throw error when only one parameter is given', () => {
   expect(() => calculator.calc('alma')).toThrow()
 })
 
 test('should throw error when first parameter cannot be translated as a date', () => {
   expect(() => calculator.calc('alma', 'korte')).toThrow()
+})
+
+test('should throw error when second parameter couldn`t is not a number', () => {
+  expect(() => calculator.calc('2012-12-12T11:32:00Z', 'korte')).toThrow()
+})
+
+test('should throw error when second parameter couldn`t be translated as positive, not zero number', () => {
+  expect(() => calculator.calc('2012-12-12T11:32:00Z', 0)).toThrow()
 })
