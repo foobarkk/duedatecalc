39828606da8cf21f5475e221cf3b406256439ecc
Katalin Kiss <katalin@ghostmonitor.com>
2018-08-22 16:15:13 +0200

    due date is on the next date when with added turnaround oferflows workday

--- src/calc.js
+++ src/calc.js
@@ -1,3 +1,6 @@
+const ENDOFWORKDAY = 17
+const STARTOFWORKDAY = 9
+
 module.exports = {
   calc: (ticketCreatedAt, turnaroundHours) => {
     if (isNaN(turnaroundHours) || !turnaroundHours > 0) throw new Error('Turnaround is not a positive number')
@@ -5,7 +8,11 @@ module.exports = {
     const startTime = new Date(ticketCreatedAt)
     if (!(startTime instanceof Date) || isNaN(startTime.valueOf())) throw new Error('First parameter must be a date')
 
-    const dueTime = new Date(startTime.setHours(startTime.getHours() + turnaroundHours))
+    let dueTime = new Date(startTime.setUTCHours(startTime.getUTCHours() + turnaroundHours))
+    if (dueTime.getUTCHours() > ENDOFWORKDAY) {
+      dueTime.setUTCDate(dueTime.getUTCDate() + 1)
+      dueTime.setUTCHours(STARTOFWORKDAY + turnaroundHours)
+    }
     return dueTime
   }
 }
--- test/calc.test.js
+++ test/calc.test.js
@@ -3,6 +3,9 @@
 
 const calculator = require('../src/calc')
 
+const dateDiffInHours = (date1, date2) => (date1.valueOf() - date2.valueOf()) / 60 / 60 / 1000
+const nightTimeInHours = 16
+
 test('should throw error when only one parameter is given', () => {
   expect(() => calculator.calc('alma')).toThrow()
 })
@@ -28,6 +31,14 @@ test('due time should be at least turnaround hours later than the ticket creatio
   const ticketCreatedAt = new Date('2018-08-22T09:12Z')
   const turnaround = 2
   const dueTime = calculator.calc(ticketCreatedAt, turnaround)
-  const diffHours = (dueTime.valueOf() - ticketCreatedAt.valueOf()) / 60 / 60 / 1000
+  const diffHours = dateDiffInHours(dueTime, ticketCreatedAt)
   expect(diffHours).toBeGreaterThanOrEqual(turnaround)
+})
+
+test('due time should continue on the next day from 9 if it would overflow 17', () => {
+  const ticketCreatedAt = new Date('2018-08-22T16:12Z')
+  const turnaround = 2
+  const dueTime = calculator.calc(ticketCreatedAt, turnaround)
+  const diffHours = dateDiffInHours(dueTime, ticketCreatedAt)
+  expect(diffHours).toBeGreaterThanOrEqual(turnaround + nightTimeInHours)
 })
