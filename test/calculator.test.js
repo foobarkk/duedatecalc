ba87c81fb93e8036a07df65279fc1c3b32132401
Katalin Kiss <katalin@ghostmonitor.com>
2018-08-23 08:30:05 +0200

    use alternate config

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -2,9 +2,16 @@
 /* global expect, test */
 
 const calculator = require('../lib/calculator')
+const config = require('../lib/calculator.config')
 
 const dateDiffInHours = (date1, date2) => (date1.valueOf() - date2.valueOf()) / 60 / 60 / 1000
-const nightTimeInHours = 16
+const getNightTimeInHours = () => {
+  const start = new Date(0).setUTCHours(config.WORKDAYEND)
+  const end = new Date(0).setUTCHours(config.WORKDAYEND + (24 - config.WORKDAYLENGTH))
+  const hours = dateDiffInHours(end, start)
+  return hours
+}
+const nightTimeInHours = getNightTimeInHours()
 const weekendHours = 48
 
 test('should throw error when only one parameter is given', () => {
@@ -60,20 +67,21 @@ test('should be able to handle more than 8 hours turnaround time', () => {
   expect(diffHours).toBe(turnaround + (nightTimeInHours * 2))
 })
 
-test('should be able to handle more than 16 hours turnaround time', () => {
+test('should be able to handle more than two workdays hours turnaround time', () => {
   const ticketCreatedAt = new Date('2018-08-13T16:12Z')
-  const turnaround = 17
+  const turnaround = config.WORKDAYLENGTH * 2 + (config.WORKDAYEND - ticketCreatedAt.getUTCHours())
   const dueTime = calculator.getDueTime(ticketCreatedAt, turnaround)
   const diffHours = dateDiffInHours(dueTime, ticketCreatedAt)
-  expect(diffHours).toBe(turnaround + (nightTimeInHours * 3))
+  expect(diffHours).toBe(turnaround + (nightTimeInHours * (parseInt(turnaround / config.WORKDAYLENGTH) + 1)))
 })
 
 test('should handle start times with more than one hour remaining on workday', () => {
   const ticketCreatedAt = new Date('2018-08-22T09:12Z')
-  const turnaround = 17
+  const overFlow = 1
+  const turnaround = config.WORKDAYLENGTH * 2 + overFlow
   const dueTime = calculator.getDueTime(ticketCreatedAt, turnaround)
   const diffHours = dateDiffInHours(dueTime, ticketCreatedAt)
-  expect(diffHours).toBe(turnaround + (nightTimeInHours * 2))
+  expect(diffHours).toBe(turnaround + (nightTimeInHours * parseInt(turnaround / config.WORKDAYLENGTH)))
 })
 
 test('should skip weekend days if due time is on Saturday or Sunday', () => {
@@ -86,15 +94,17 @@ test('should skip weekend days if due time is on Saturday or Sunday', () => {
 
 test('should skip two weekends', () => {
   const ticketCreatedAt = new Date('2018-08-22T16:12Z')
-  const turnaround = 66
+  const overFlow = 2
+  const days = 8
+  const turnaround = config.WORKDAYLENGTH * days + overFlow
   const dueTime = calculator.getDueTime(ticketCreatedAt, turnaround)
   const diffHours = dateDiffInHours(dueTime, ticketCreatedAt)
-  expect(diffHours).toBe(turnaround + (weekendHours * 2) + nightTimeInHours * 9)
+  expect(diffHours).toBe(turnaround + (weekendHours * 2) + nightTimeInHours * (days + 1))
 })
 
 test('should skip more than two weekends', () => {
   const ticketCreatedAt = new Date('2018-08-22T09:12Z')
-  const turnaround = 105
+  const turnaround = config.WORKDAYLENGTH * 13 + 1
   const dueTime = calculator.getDueTime(ticketCreatedAt, turnaround)
   const diffHours = dateDiffInHours(dueTime, ticketCreatedAt)
   expect(diffHours).toBe(turnaround + (weekendHours * 3) + nightTimeInHours * 13)
@@ -102,24 +112,27 @@ test('should skip more than two weekends', () => {
 
 test('should skip more than two weekends with friday ending', () => {
   const ticketCreatedAt = new Date('2018-08-22T09:12Z')
-  const turnaround = 96
+  const days = 12
+  const turnaround = config.WORKDAYLENGTH * days
   const dueTime = calculator.getDueTime(ticketCreatedAt, turnaround)
   const diffHours = dateDiffInHours(dueTime, ticketCreatedAt)
-  expect(diffHours).toBe(turnaround + (weekendHours * 2) + nightTimeInHours * 12)
+  expect(diffHours).toBe(turnaround + (weekendHours * 2) + nightTimeInHours * days)
 })
 
 test('should skip more than two weekends with friday ending and day ending', () => {
   const ticketCreatedAt = new Date('2018-08-22T16:12Z')
-  const turnaround = 56
+  const days = 7
+  const turnaround = config.WORKDAYLENGTH * days
   const dueTime = calculator.getDueTime(ticketCreatedAt, turnaround)
   const diffHours = dateDiffInHours(dueTime, ticketCreatedAt)
-  expect(diffHours).toBe(turnaround + (weekendHours) + nightTimeInHours * 7)
+  expect(diffHours).toBe(turnaround + (weekendHours) + nightTimeInHours * days)
 })
 
 test('should skip right amout of weekdays even if starts on Monday 9AM', () => {
   const ticketCreatedAt = new Date('2018-08-20T09:00Z')
-  const turnaround = 88
+  const days = 11
+  const turnaround = config.WORKDAYLENGTH * days
   const dueTime = calculator.getDueTime(ticketCreatedAt, turnaround)
   const diffHours = dateDiffInHours(dueTime, ticketCreatedAt)
-  expect(diffHours).toBe(turnaround + (weekendHours * 2) + nightTimeInHours * 11)
+  expect(diffHours).toBe(turnaround + (weekendHours * 2) + nightTimeInHours * days)
 })
